3b5db86a1834ac81ac1f9a1805857620c55303bd
ayappa <aayappareddy@gmail.com>
2018-03-15 22:35:54 +0530

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,40 @@
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    };
+    console.log('Connected to MongoDB server');
+
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID("5aaa9ba27bfc700ad6d2f5ba")
+    // }, {
+    //    $set: {
+    //        completed: true
+    //    } 
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        name: 'Deepak'
+    }, {
+        $set: {
+            name: 'Ayappa'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+    
+    // client.close();
+});
