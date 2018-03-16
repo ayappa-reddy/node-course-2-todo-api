dccf76b9b6403f7b6c9f53e05cecc241f006d6cb
ayappa <aayappareddy@gmail.com>
2018-03-16 21:14:49 +0530

    Modifying mongoose.js for heroku deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ const {ObjectID} = require('mongodb');
 mongoose.Promise = global.Promise;
 let db = {
     localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://ayappa:Dxkillo619@ds149268.mlab.com:49268/todoapp'
+    mlab: 'mongodb://ayappa:Dxkillo619@ds113505.mlab.com:13505/todo-app-db'
 };
 mongoose.connect(process.env.PORT ? db.localhost : db.mlab);
 
