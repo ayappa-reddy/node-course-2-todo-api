444746bbad7468626a12586af52a2b9fa1655576
ayappa <aayappareddy@gmail.com>
2018-03-16 21:27:33 +0530

    Setting up MONGODB_URI for heroku deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 const {ObjectID} = require('mongodb');
 
 mongoose.Promise = global.Promise;
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://ayappa:Dxkillo619@ds113505.mlab.com:13505/todo-app-db'
-};
-mongoose.connect(process.env.PORT ? db.localhost : db.mlab);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose, ObjectID};
